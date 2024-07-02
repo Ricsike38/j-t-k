9b53de44cc52b592969bbac796612c98cfc9b033
Ricsike38 <106070965+Ricsike38@users.noreply.github.com>
2024-07-02 12:27:14 +0200

    Add files via upload

---
+++ Játék2/jatek_datumokkal_raadas/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace jatek_datumokkal_raadas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTalalkozo = new System.Windows.Forms.Label();
+            this.lblAktualis = new System.Windows.Forms.Label();
+            this.dtTmPckrDatum = new System.Windows.Forms.DateTimePicker();
+            this.dtTmPckrIdo = new System.Windows.Forms.DateTimePicker();
+            this.lblErtekeles = new System.Windows.Forms.Label();
+            this.btnErtekel = new System.Windows.Forms.Button();
+            this.btnBezar = new System.Windows.Forms.Button();
+            this.btnTorol = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A találkozó ideje: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "A találkozó ideje: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Aktuális idő:";
+            // 
+            // lblTalalkozo
+            // 
+            this.lblTalalkozo.AutoSize = true;
+            this.lblTalalkozo.Location = new System.Drawing.Point(173, 102);
+            this.lblTalalkozo.Name = "lblTalalkozo";
+            this.lblTalalkozo.Size = new System.Drawing.Size(35, 13);
+            this.lblTalalkozo.TabIndex = 3;
+            this.lblTalalkozo.Text = "label4";
+            // 
+            // lblAktualis
+            // 
+            this.lblAktualis.AutoSize = true;
+            this.lblAktualis.Location = new System.Drawing.Point(173, 133);
+            this.lblAktualis.Name = "lblAktualis";
+            this.lblAktualis.Size = new System.Drawing.Size(35, 13);
+            this.lblAktualis.TabIndex = 4;
+            this.lblAktualis.Text = "label5";
+            // 
+            // dtTmPckrDatum
+            // 
+            this.dtTmPckrDatum.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtTmPckrDatum.Location = new System.Drawing.Point(25, 60);
+            this.dtTmPckrDatum.Name = "dtTmPckrDatum";
+            this.dtTmPckrDatum.Size = new System.Drawing.Size(90, 20);
+            this.dtTmPckrDatum.TabIndex = 5;
+            // 
+            // dtTmPckrIdo
+            // 
+            this.dtTmPckrIdo.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtTmPckrIdo.Location = new System.Drawing.Point(176, 60);
+            this.dtTmPckrIdo.Name = "dtTmPckrIdo";
+            this.dtTmPckrIdo.ShowUpDown = true;
+            this.dtTmPckrIdo.Size = new System.Drawing.Size(75, 20);
+            this.dtTmPckrIdo.TabIndex = 6;
+            // 
+            // lblErtekeles
+            // 
+            this.lblErtekeles.AutoSize = true;
+            this.lblErtekeles.Font = new System.Drawing.Font("Microsoft YaHei", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblErtekeles.ForeColor = System.Drawing.Color.Red;
+            this.lblErtekeles.Location = new System.Drawing.Point(87, 183);
+            this.lblErtekeles.Name = "lblErtekeles";
+            this.lblErtekeles.Size = new System.Drawing.Size(59, 22);
+            this.lblErtekeles.TabIndex = 7;
+            this.lblErtekeles.Text = "label6";
+            // 
+            // btnErtekel
+            // 
+            this.btnErtekel.Location = new System.Drawing.Point(28, 219);
+            this.btnErtekel.Name = "btnErtekel";
+            this.btnErtekel.Size = new System.Drawing.Size(75, 23);
+            this.btnErtekel.TabIndex = 8;
+            this.btnErtekel.Text = "Értékel";
+            this.btnErtekel.UseVisualStyleBackColor = true;
+            this.btnErtekel.Click += new System.EventHandler(this.btnErtekel_Click);
+            // 
+            // btnBezar
+            // 
+            this.btnBezar.Location = new System.Drawing.Point(107, 258);
+            this.btnBezar.Name = "btnBezar";
+            this.btnBezar.Size = new System.Drawing.Size(75, 23);
+            this.btnBezar.TabIndex = 9;
+            this.btnBezar.Text = "Bezár";
+            this.btnBezar.UseVisualStyleBackColor = true;
+            this.btnBezar.Click += new System.EventHandler(this.btnBezar_Click);
+            // 
+            // btnTorol
+            // 
+            this.btnTorol.Location = new System.Drawing.Point(176, 219);
+            this.btnTorol.Name = "btnTorol";
+            this.btnTorol.Size = new System.Drawing.Size(75, 23);
+            this.btnTorol.TabIndex = 10;
+            this.btnTorol.Text = "Töröl";
+            this.btnTorol.UseVisualStyleBackColor = true;
+            this.btnTorol.Click += new System.EventHandler(this.btnTorol_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 376);
+            this.Controls.Add(this.btnTorol);
+            this.Controls.Add(this.btnBezar);
+            this.Controls.Add(this.btnErtekel);
+            this.Controls.Add(this.lblErtekeles);
+            this.Controls.Add(this.dtTmPckrIdo);
+            this.Controls.Add(this.dtTmPckrDatum);
+            this.Controls.Add(this.lblAktualis);
+            this.Controls.Add(this.lblTalalkozo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Találkozó";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTalalkozo;
+        private System.Windows.Forms.Label lblAktualis;
+        private System.Windows.Forms.DateTimePicker dtTmPckrDatum;
+        private System.Windows.Forms.DateTimePicker dtTmPckrIdo;
+        private System.Windows.Forms.Label lblErtekeles;
+        private System.Windows.Forms.Button btnErtekel;
+        private System.Windows.Forms.Button btnBezar;
+        private System.Windows.Forms.Button btnTorol;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
